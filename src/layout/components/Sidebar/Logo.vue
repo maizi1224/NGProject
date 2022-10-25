aa48377b5e503cb656e85d64fa60cc81e772a8ec
maizi <851620279@qq.com>
2022-10-26 01:49:24 +0800

    修改网站标题

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: maizi 851620279@qq.com
+ * @Date: 2022-10-08 02:20:47
+ * @LastEditors: maizi 851620279@qq.com
+ * @LastEditTime: 2022-10-26 01:49:10
+ * @FilePath: \NGProject\src\layout\components\Sidebar\Logo.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
   <div class="sidebar-logo-container" :class="{'collapse':collapse}">
     <transition name="sidebarLogoFade">
@@ -24,7 +32,7 @@ export default {
   },
   data() {
     return {
-      title: 'RuYiAdmin',
+      title: '宁国国控',
       logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
     }
   }
